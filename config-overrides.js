187c0c9529d8af524a6ec69b561c4520184012e1
gabrielburich <gabrielburich@gmail.com>
2020-03-06 21:09:16 -0300

    [Craco] - Removido react app rewired

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-const {override, addWebpackAlias} = require("customize-cra");
-const path = require("path");
-
-module.exports = override(
-    // Custom Alias
-    addWebpackAlias({
-        ['@commons']: path.resolve(__dirname, 'src', 'components', 'commons')
-    }),
-);
